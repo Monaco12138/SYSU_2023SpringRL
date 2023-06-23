d9c9167e9a4e9880a279ee69c8d3cedd95ea1459
Monaco12138 <1658966374@qq.com>
2023-06-23 17:14:42 +0800

    add agents/MADDPG

---
+++ Assignment2/agents/MADDPG/submission.py
@@ -0,0 +1,104 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+from utils.make_env import make_env
+import os
+
+
+def onehot_from_logits(logits, eps=0.01):
+    ''' 生成最优动作的独热（one-hot）形式 '''
+    argmax_acs = (logits == logits.max(1, keepdim=True)[0]).float()
+    # 生成随机动作,转换成独热形式
+    rand_acs = torch.autograd.Variable(torch.eye(logits.shape[1])[[
+        np.random.choice(range(logits.shape[1]), size=logits.shape[0])
+    ]],
+                                       requires_grad=False).to(logits.device)
+    # 通过epsilon-贪婪算法来选择用哪个动作
+    return torch.stack([
+        argmax_acs[i] if r > eps else rand_acs[i]
+        for i, r in enumerate(torch.rand(logits.shape[0]))
+    ])
+
+
+def sample_gumbel(shape, eps=1e-20, tens_type=torch.FloatTensor):
+    """从Gumbel(0,1)分布中采样"""
+    U = torch.autograd.Variable(tens_type(*shape).uniform_(),
+                                requires_grad=False)
+    return -torch.log(-torch.log(U + eps) + eps)
+
+
+def gumbel_softmax_sample(logits, temperature):
+    """ 从Gumbel-Softmax分布中采样"""
+    y = logits + sample_gumbel(logits.shape, tens_type=type(logits.data)).to(
+        logits.device)
+    return F.softmax(y / temperature, dim=1)
+
+
+def gumbel_softmax(logits, temperature=1.0):
+    """从Gumbel-Softmax分布中采样,并进行离散化"""
+    y = gumbel_softmax_sample(logits, temperature)
+    y_hard = onehot_from_logits(y)
+    y = (y_hard.to(logits.device) - y).detach() + y
+    # 返回一个y_hard的独热量,但是它的梯度是y,我们既能够得到一个与环境交互的离散动作,又可以
+    # 正确地反传梯度
+    return y
+
+class TwoLayerFC(torch.nn.Module):
+    def __init__(self, num_in, num_out, hidden_dim):
+        super().__init__()
+        self.fc1 = torch.nn.Linear(num_in, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = torch.nn.Linear(hidden_dim, num_out)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
+
+
+ENV = make_env('simple_spread', discrete_action=True)
+N_AGENT = 3
+N_ACTION = ENV.action_space[0].n  # 5
+N_OBS = ENV.observation_space[0].shape[0]  # 18
+
+class DDPG:
+    ''' DDPG算法 '''
+    def __init__(self, state_dim, action_dim, hidden_dim, device):
+        
+        self.actor = TwoLayerFC(state_dim, action_dim, hidden_dim).to(device)
+        
+
+    def take_action(self, state, explore=False):
+        action = self.actor(state)
+        if explore:
+            action = gumbel_softmax(action)
+        else:
+            action = onehot_from_logits(action)
+        return action.detach().cpu().numpy()[0]
+
+class Agents:
+    def __init__( self, agent_num=N_AGENT, hidden_dim=64 ):
+        
+        self.agent_num = agent_num
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.agents = []
+        for i in range( agent_num ):
+            self.agents.append(
+                DDPG(N_OBS, N_ACTION, hidden_dim, self.device))
+        
+        self.critic_criterion = torch.nn.MSELoss()
+
+    def act(self, states, explore=False):
+        states = [
+            torch.tensor([states[i]], dtype=torch.float, device=self.device)
+            for i in range( self.agent_num )
+        ]
+        return [
+            agent.take_action(state, explore)
+            for agent, state in zip(self.agents, states)
+        ]
+
+    def load_parameters( self, save_name ):
+        for i in range( len(self.agents) ):
+            self.agents[i].actor.load_state_dict( torch.load(os.path.join( '/home/ubuntu/main/2023RL/SYSU_2023SpringRL2/Assignment2/model', '{}_agent{}.pth'.format(save_name, i)) ) )
+            
--- Assignment2/run_test.py
+++ Assignment2/run_test.py
@@ -2,23 +2,34 @@ import argparse
 import time
 import numpy as np
 from utils.make_env import make_env
+from PIL import Image
 
 # TODO: replace with your models
 from agents.random.submission import Agents as RandomSampleAgents
 from agents.random_network.submission import Agents as RandomNetworkAgents
-
+from agents.MADDPG.submission import Agents as MADDPG
+import os
 
 def run(config):
     env = make_env(config.env_id, discrete_action=True)
 
     # TODO: replace with you own agent model
     #agents = RandomNetworkAgents(env.observation_space[0].shape[0], env.action_space[0].n)
-    agents = RandomSampleAgents()
+    #agents = RandomSampleAgents()
+    agents = MADDPG()
+    agents.load_parameters( 'maddpg_check50000' )
+
     total_reward = 0.
+    
+    img_list = [ ]
     for ep_i in range(config.n_episodes):
         print("Episode %i of %i" % (ep_i + 1, config.n_episodes))
+        trace_list = []
+
         obs = env.reset()
-        # env.render('human')
+        
+        trace_list.append( Image.fromarray( env.render('rgb_array')[0] ) )
+
         episode_reward = 0.
         for t_i in range(config.episode_length):
             calc_start = time.time()
@@ -27,20 +38,27 @@ def run(config):
             
             #obs: 3*(18,)
             #actions 3*(5,)
-            print( actions )
+            #print( actions )
             obs, rewards, dones, infos = env.step(actions)
             episode_reward += np.array(rewards).sum()
             calc_end = time.time()
             elapsed = (calc_end - calc_start) * 1000.0
+
             # the elapsed should not exceed 10ms per step
-            #print("Elapsed %f ms" % (elapsed))
-            # env.render('human')
+            print("Elapsed %f ms" % (elapsed))
+
+            # add render result
+            trace_list.append( Image.fromarray( env.render('rgb_array')[0] ) )
         total_reward += episode_reward/config.episode_length
         print("Episode reward: %.2f" % (episode_reward/config.episode_length))
+
+        img_list.append( trace_list )
         
     print("Mean reward of %d episodes: %.2f" % (config.n_episodes, total_reward/config.n_episodes))
 
     env.close()
+
+    return img_list
 
 
 if __name__ == '__main__':
@@ -50,4 +68,14 @@ if __name__ == '__main__':
     parser.add_argument("--episode_length", default=25, type=int)
     config = parser.parse_args()
 
-    run(config)
+    img_list = run(config)
+    idx = 0
+    for i in range( config.n_episodes ):
+        for j in range( len(img_list[i]) ):
+            img = img_list[i][j]
+
+            img.save('./photo/img_{}.png'.format( idx ) )
+            idx += 1
+    
+
+    #os.system( 'ffmpeg -r 10 -f image2 -i ./photo/img_%d.png output.mp4' )
